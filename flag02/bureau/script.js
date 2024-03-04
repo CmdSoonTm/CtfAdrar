7e24127c5886d29f93fee1df1061b794a947cc90
CmdSoonTm <ty.it@outlook.fr>
2024-03-04 21:02:44 +0000

    modified:   flag02/bureau/script.js 	modified:   flag02/computer/script.js

--- flag02/bureau/script.js
+++ flag02/bureau/script.js
@@ -1,7 +1,8 @@
 function updateDateTime() {
     var now = new Date();
+    now.setFullYear(now.getFullYear() + 100);
     var datetime = document.getElementById('datetime');
-    datetime.textContent = now.toLocaleDateString() + '   ' + now.toLocaleTimeString();
+    datetime.textContent = now.toLocaleDateString('fr-FR', { day: 'numeric', month: 'long', year: 'numeric' }) + '   ' + now.toLocaleTimeString();
 }
 setInterval(updateDateTime, 1000);
 
